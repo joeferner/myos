6812c5510687f6e85201a91bdca5ad3120ab7533
Joe Ferner <joe@fernsroth.com>
2025-07-14 22:47:58 -0400

    wip

--- kernel/src/pci.rs
+++ kernel/src/pci.rs
@@ -1,3 +1,4 @@
+use bit_field::BitField;
 use spin::mutex::Mutex;
 use x86_64::instructions::port::{PortGeneric, ReadWriteAccess};
 
@@ -10,84 +11,109 @@ const CONFIG_DATA: u16 = 0xcfc;
 const CONFIG_DATA_PORT: Mutex<PortGeneric<u32, ReadWriteAccess>> =
     Mutex::new(PortGeneric::<u32, ReadWriteAccess>::new(CONFIG_DATA));
 
-#[repr(C, packed)]
-struct PciCommonHeader {
-    pub vendor_id: u16,
-    pub device_id: u16,
-    pub command: u16,
-    pub status: u16,
-    pub revision_id: u8,
-    pub prog_if: u8,
-    pub subclass: u8,
-    pub class_code: u8,
-    pub cache_line_size: u8,
-    pub latency_timer: u8,
-    pub header_type: u8,
-    pub bist: u8,
+/// The address of a PCIe function.
+///
+/// PCIe supports 65536 segments, each with 256 buses, each with 32 slots, each with 8 possible functions.:
+///
+/// ```ignore
+/// 32                              16               8         3      0
+///  +-------------------------------+---------------+---------+------+
+///  |            segment            |      bus      | device  | func |
+///  +-------------------------------+---------------+---------+------+
+/// ```
+struct PciAddress(u32);
+
+impl PciAddress {
+    pub fn new(bus: u8, device: u8, func: u8, offset: u8) -> Self {
+        let bus: u32 = bus.into();
+        let slot: u32 = device.into();
+        let func: u32 = func.into();
+        let offset: u32 = offset.into();
+        let address: u32 = (bus << 16) | (slot << 11) | (func << 8) | (offset & 0xfc) | 0x80000000;
+        Self(address)
+    }
+}
+
+pub type VendorId = u16;
+pub type DeviceId = u16;
+pub type HasMultipleFunctions = bool;
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum HeaderType {
+    Endpoint,
+    PciPciBridge,
+    CardBusBridge,
+    Unknown(u8),
+}
+
+/// ```ignore
+///       31          24 23         16 15          8 7                0
+///       +-------------+-------------+-------------+-----------------+
+/// 0x00  |        Device ID          |         Vendor ID             |
+/// 0x04  |          Status           |          Command              |
+/// 0x08  |        Class Code                       | Revision ID     |
+/// 0x0c  |     0x00    | Header Type |  0x00       | Cache Line Size |
+///       +-------------+-------------+-------------+-----------------+
+/// ```
+struct PciCommonHeader(PciAddress);
+
+impl PciCommonHeader {
+    pub const fn new(address: PciAddress) -> Self {
+        Self(address)
+    }
+
+    pub fn id(&self) -> Option<(VendorId, DeviceId)> {
+        let data = pci_read(&self.0, 0x00);
+        if data == 0xffff_ffff {
+            return None;
+        }
+        let vendor_id = data.get_bits(0..16) as VendorId;
+        let device_id = data.get_bits(16..32) as DeviceId;
+        Some((vendor_id, device_id))
+    }
+
+    pub fn header_type(&self) -> (HasMultipleFunctions, HeaderType) {
+        let data = pci_read(&self.0, 0x0c);
+        // high level bit 23 contains
+        let header_type = data.get_bits(16..23);
+        let header_type = match header_type {
+            0x00 => HeaderType::Endpoint,
+            0x01 => HeaderType::PciPciBridge,
+            0x02 => HeaderType::CardBusBridge,
+            v => HeaderType::Unknown(v as u8),
+        };
+        let has_multiple_functions = data.get_bit(23);
+        (has_multiple_functions, header_type)
+    }
 }
 
 pub fn pci_enumerate() {
     for bus in 0..=255 {
         for device in 0..32 {
-            if let Some(vendor) = pci_read_vendor(bus, device) {
-                let device_id = pci_read_device(bus, device).unwrap();
-                println!("{bus}:{device} => {vendor:x} {device_id:x}");
-                let header_type = pci_read_header_type(bus, device, 0);
+            let header = PciCommonHeader::new(PciAddress::new(bus, device, 0, 0));
+            if let Some((vendor_id, device_id)) = header.id() {
+                let (has_multiple_functions, header_type) = header.header_type();
+                println!("{bus}:{device}.0 => {vendor_id:x} {device_id:x} {header_type:?}");
+                if has_multiple_functions {
+                    for function in 1..8 {
+                        let header =
+                            PciCommonHeader::new(PciAddress::new(bus, device, function, 0));
+                        if let Some((vendor_id, device_id)) = header.id() {
+                            println!(
+                                "  {bus}:{device}.{function} => {vendor_id:x} {device_id:x}"
+                            );
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-fn pci_read_vendor(bus: u8, device: u8) -> Option<u16> {
-    let vendor = pic_config_read_word(bus, device, 0, 0);
-    if vendor == 0xffff { None } else { Some(vendor) }
-}
-
-fn pci_read_device(bus: u8, device: u8) -> Option<u16> {
-    let device = pic_config_read_word(bus, device, 0, 2);
-    if device == 0xffff { None } else { Some(device) }
-}
-
-fn pci_read_header(bus: u8, device: u8) -> Option<PciCommonHeader> {
-    let vendor_id = pic_config_read_word(bus, device, 0, 0);
-    if vendor_id == 0xffff {
-        return None;
-    }
-
-    let device_id = pic_config_read_word(bus, device, 0, 2);
-    if device_id == 0xffff {
-        return None;
-    }
-
-    Some(PciCommonHeader {
-        vendor_id,
-        device_id,
-        command,
-        status,
-        revision_id,
-        prog_if,
-        subclass,
-        class_code,
-        cache_line_size,
-        latency_timer,
-        header_type,
-        bist,
-    })
-}
-
-fn pic_config_read_word(bus: u8, device: u8, func: u8, offset: u8) -> u16 {
-    let bus: u32 = bus.into();
-    let slot: u32 = device.into();
-    let func: u32 = func.into();
-    let offset: u32 = offset.into();
-
-    let address: u32 = (bus << 16) | (slot << 11) | (func << 8) | (offset & 0xfc) | 0x80000000;
-
+fn pci_read(address: &PciAddress, offset: u32) -> u32 {
+    let address: u32 = address.0 + offset;
     unsafe {
         CONFIG_ADDRESS_PORT.lock().write(address);
     }
-
-    let r = unsafe { CONFIG_DATA_PORT.lock().read() };
-
-    return ((r >> ((offset & 2) * 8)) & 0xffff) as u16;
+    unsafe { CONFIG_DATA_PORT.lock().read() }
 }
